f68e247ad6516cd0b306994f9ef6c6dbf53b5229
Tomas <tomascanepa1397@gmail.com>
2024-04-24 17:45:05 -0300

    fixed formatting to pass CI

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ async fn greet(req: HttpRequest) -> impl Responder {
 async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
-        .route("/", web::get().to(greet))
-        .route("/{name}", web::get().to(greet))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
