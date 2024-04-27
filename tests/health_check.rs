e8ea6eb316f46b4fd34b5df2ba53e4a37f5d0056
Tomas <tomascanepa1397@gmail.com>
2024-04-27 18:09:36 -0300

    connected to database. Tests now fail (which is expected behavior for now)

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
+use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::test]
@@ -32,6 +34,11 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subscribe_200_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
     let client = reqwest::Client::new();
 
     let body = "name=charles%20leclerc&email=charles.leclerc%40gmail.com";
@@ -44,6 +51,14 @@ async fn subscribe_200_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "charles.leclerc@gmail.com");
+    assert_eq!(saved.name, "charles leclerc");
 }
 
 #[tokio::test]
