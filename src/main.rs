12b7bc2d588bae5f03541d780264bfceac1deb68
Tomas <tomascanepa1397@gmail.com>
2024-04-27 16:32:00 -0300

    added configuration.yaml file, made a get_configuration function in configuration.rs and modified main.rs to read and use the configuration from the yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::net::TcpListener;
 
-use newsletter::startup::run;
+use newsletter::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    // Read configuration and panic if it's not possible
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
