ff444b31bf034db782a1c3f62b95b6d67831b1b6
Tomas <tomascanepa1397@gmail.com>
2024-04-28 13:33:47 -0300

    POST /subscribe first implementation, tests create a new database, and replace PgConnect for PgPool to be able to handle multiple queies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use std::net::TcpListener;
-
 use newsletter::{configuration::get_configuration, startup::run};
+use sqlx::PgPool;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Read configuration and panic if it's not possible
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
