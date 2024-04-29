e2a055ff1635a265792c1ddaa497a5efd28b2cd3
Tomas <tomascanepa1397@gmail.com>
2024-04-29 17:52:00 -0300

    Added telemetry to tests

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Logging
-    let subscriber = get_subscriber("newsletter".into(), "info".into());
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // Read configuration and panic if it's not possible
