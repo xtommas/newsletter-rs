8b9a6f5b809ae0b76210eaed487f22af86e96c3f
Tomas <tomascanepa1397@gmail.com>
2024-04-29 17:20:08 -0300

    Added actix_web logs and removed unused dependencies from Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Logging
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
     let subscriber = Registry::default()
