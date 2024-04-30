ae1d7556d801d179f099ece2b93cc2c2753f4866
Tomas <tomascanepa1397@gmail.com>
2024-04-30 15:57:10 -0300

    Wrapped sensitive values with Secret

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use newsletter::{
     configuration::get_configuration, startup::run, telemetry::get_subscriber,
     telemetry::init_subscriber,
 };
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -13,9 +14,10 @@ async fn main() -> Result<(), std::io::Error> {
 
     // Read configuration and panic if it's not possible
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
